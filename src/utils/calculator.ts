6742988a0cd39cf9e65ff005c958f1800ce57456
João Bispo <joaobispo2077@gmail.com>
2021-10-17 18:27:53 -0300

    feat: add calculate and get operation by symbol function into calculator utils

---
+++ src/utils/calculator.ts
@@ -0,0 +1,82 @@
+type MathExpressionSymbols = 'x' | '/' | '+' | '-';
+
+const expressionSymbols = ['+', '-', 'x', '/', ','];
+
+function getOperationBySymbol(symbol: MathExpressionSymbols) {
+  type MathOperation = (a: number, b: number) => number;
+
+  const multi: MathOperation = (a, b) => a * b;
+  const divide: MathOperation = (a, b) => a / b;
+  const sum: MathOperation = (a, b) => a + b;
+  const subtract: MathOperation = (a, b) => a - b;
+
+  const mathOperationBySymbol: Record<MathExpressionSymbols, MathOperation> = {
+    x: multi,
+    '/': divide,
+    '+': sum,
+    '-': subtract,
+  };
+
+  return mathOperationBySymbol[symbol];
+}
+
+function calculate(expression: string) {
+  const satinizedExpression = expression.replace(/,/, '.').replace(/,/, '');
+
+  const foundFirstSymbol = Array.from(satinizedExpression).find(
+    (symbol, index) => expressionSymbols.includes(symbol) && index !== 0
+  );
+
+  const mathOperate = getOperationBySymbol(
+    foundFirstSymbol as MathExpressionSymbols
+  );
+
+  const [firstNumber, secondNumber] = satinizedExpression.split(
+    foundFirstSymbol as MathExpressionSymbols
+  );
+
+  const result = mathOperate(Number(firstNumber), Number(secondNumber));
+
+  return result.toString().replace('.', ',');
+}
+
+// function isValidExpression(expression: string): boolean {
+
+//   const candidateText = expression;
+
+//   const lastValue = candidateText.slice(-1);
+
+//   const isCandidateRepeatSymbol =
+//     expressionSymbols.includes(lastValue) &&
+//     expressionSymbols.includes(candidateText);
+//   const validExpressionPattern = /^(((\d+)|-|\+|x|\/|(?:x)|(,))+)$/g;
+//   const isValidExpression =
+//     validExpressionPattern.test(candidateText) && !isCandidateRepeatSymbol;
+//   const invalidSymbolsAsFirstValue = ['x', '/'];
+//   const isValidFirstValue =
+//     isValidExpression && !invalidSymbolsAsFirstValue.includes(candidateText);
+//   if (isEmptyExpression.value) {
+//     if (isValidFirstValue) {
+//       expression.value += candidateText;
+//     }
+//     return;
+//   }
+//   // dont touch this, idk why it works
+//   const hasTwoFactors =
+//     Array.from(expression.value).find(
+//       (symbol, index) => expressionSymbols.includes(symbol) && index !== 0
+//     ) && expressionSymbols.includes(candidateText);
+//   if (hasTwoFactors) {
+//     return;
+//   }
+//   if (isValidExpression) {
+//     expression.value += candidateText;
+//     return;
+//   }
+// }
+export {
+  calculate,
+  getOperationBySymbol,
+  expressionSymbols,
+  MathExpressionSymbols,
+};
