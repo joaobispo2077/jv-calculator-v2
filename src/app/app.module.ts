cf570da42815e9f4c6ad3fe8737d6d6aa4429218
João Bispo <joaobispo2077@gmail.com>
2021-10-17 18:12:03 -0300

    feat: add styles of calculator component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CalculatorComponent } from './calculator/calculator.component';
+import { CalculatorComponent } from './components/calculator/calculator.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CalculatorComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CalculatorComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
