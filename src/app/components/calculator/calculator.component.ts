b9ae50b95d577980cd42eb768bc07594f97e03c1
João Bispo <joaobispo2077@gmail.com>
2021-10-19 22:32:22 -0300

    feat(wip): adding validation from expression value

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { isValidExpression } from 'src/utils/calculator';
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
@@ -18,51 +19,9 @@ export class CalculatorComponent {
   }
 
   addExpressionValue(value: string) {
-    const newExpression = `${this.expression}${value}`;
+    if (isValidExpression(this.expression, value)) {
+      const newExpression = `${this.expression}${value}`;
 
-    const isValidExpression = (candidateText: string): boolean => {
-      const expressionSymbols = ['+', '-', 'x', '/', ','];
-
-      const lastValue = this.expression.slice(-1);
-
-      const isCandidateRepeatSymbol =
-        expressionSymbols.includes(lastValue) &&
-        expressionSymbols.includes(candidateText);
-
-      const validExpressionPattern = /^(((\d+)|-|\+|x|\/|(?:x)|(,))+)$/g;
-      const isValidExpression =
-        validExpressionPattern.test(candidateText) && !isCandidateRepeatSymbol;
-
-      const invalidSymbolsAsFirstValue = ['x', '/'];
-
-      const isValidFirstValue =
-        isValidExpression &&
-        !invalidSymbolsAsFirstValue.includes(candidateText);
-
-      // if (this.isEmptyExpression()) {
-      //   if (isValidFirstValue) {
-      //     this.expression += candidateText;
-      //   }
-
-      //   return true;
-      // }
-      // dont touch this, idk why it works
-      const hasTwoFactors =
-        Array.from(this.expression).find(
-          (symbol, index) => expressionSymbols.includes(symbol) && index !== 0
-        ) && expressionSymbols.includes(candidateText);
-
-      // if (hasTwoFactors) {
-      //   return;
-      // }
-      // if (isValidExpression) {
-      //   expression.value += candidateText;
-      //   return;
-      // }
-      return true;
-    };
-
-    if (isValidExpression(newExpression)) {
       this.expression = newExpression;
     }
   }
--- src/utils/calculator.ts
+++ src/utils/calculator.ts
@@ -35,38 +35,40 @@ function calculate(expression: string) {
   return result.toString().replace('.', ',');
 }
 
-function isValidExpression(expression: string): boolean {
-  // const candidateText = expression;
-  // const lastValue = candidateText.slice(-1);
-  // const isCandidateRepeatSymbol =
-  //   expressionSymbols.includes(lastValue) &&
-  //   expressionSymbols.includes(candidateText);
-  // const validExpressionPattern = /^(((\d+)|-|\+|x|\/|(?:x)|(,))+)$/g;
-  // const isValidExpression =
-  //   validExpressionPattern.test(candidateText) && !isCandidateRepeatSymbol;
-  // const invalidSymbolsAsFirstValue = ['x', '/'];
-  // const isValidFirstValue =
-  //   isValidExpression && !invalidSymbolsAsFirstValue.includes(candidateText);
-  // if (isEmptyExpression.value) {
-  //   if (isValidFirstValue) {
-  //     expression.value += candidateText;
-  //   }
-  //   return;
-  // }
-  // // dont touch this, idk why it works
-  // const hasTwoFactors =
-  //   Array.from(expression.value).find(
-  //     (symbol, index) => expressionSymbols.includes(symbol) && index !== 0
-  //   ) && expressionSymbols.includes(candidateText);
-  // if (hasTwoFactors) {
-  //   return;
-  // }
-  // if (isValidExpression) {
-  //   expression.value += candidateText;
-  //   return;
-  // }
-  return true;
-}
+const isValidExpression = (
+  expression: string,
+  candidateText: string
+): boolean => {
+  const expressionSymbols = ['+', '-', 'x', '/', ','];
+
+  const lastValue = expression.slice(-1);
+
+  const isCandidateRepeatSymbol =
+    expressionSymbols.includes(lastValue) &&
+    expressionSymbols.includes(candidateText);
+
+  const validExpressionPattern = /^(((\d+)|-|\+|x|\/|(?:x)|(,))+)$/g;
+  const isValidExpression =
+    validExpressionPattern.test(candidateText) && !isCandidateRepeatSymbol;
+
+  const invalidSymbolsAsFirstValue = ['x', '/'];
+
+  const isValidFirstValue =
+    isValidExpression && !invalidSymbolsAsFirstValue.includes(candidateText);
+
+  const isEmptyExpression = expression === '';
+  if (isEmptyExpression) {
+    return isValidFirstValue;
+  }
+
+  const hasTwoFactors =
+    !!Array.from(expression).find(
+      (expressionValue, index) =>
+        expressionSymbols.includes(expressionValue) && index !== 0
+    ) && !expressionSymbols.includes(candidateText);
+
+  return !!isValidExpression;
+};
 export {
   calculate,
   getOperationBySymbol,
