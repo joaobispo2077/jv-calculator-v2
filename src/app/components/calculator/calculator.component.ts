9df323374193ecb2d67f1e1533d035a315eadefc
João Bispo <joaobispo2077@gmail.com>
2021-10-19 22:05:16 -0300

    feat(wip): adding validation from expression value

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { isValidExpression } from 'src/utils/calculator';
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
@@ -14,8 +13,54 @@ export class CalculatorComponent {
     alert('calculate');
   }
 
+  isEmptyExpression(): boolean {
+    return this.expression === '';
+  }
+
   addExpressionValue(value: string) {
     const newExpression = `${this.expression}${value}`;
+
+    const isValidExpression = (candidateText: string): boolean => {
+      const expressionSymbols = ['+', '-', 'x', '/', ','];
+
+      const lastValue = this.expression.slice(-1);
+
+      const isCandidateRepeatSymbol =
+        expressionSymbols.includes(lastValue) &&
+        expressionSymbols.includes(candidateText);
+
+      const validExpressionPattern = /^(((\d+)|-|\+|x|\/|(?:x)|(,))+)$/g;
+      const isValidExpression =
+        validExpressionPattern.test(candidateText) && !isCandidateRepeatSymbol;
+
+      const invalidSymbolsAsFirstValue = ['x', '/'];
+
+      const isValidFirstValue =
+        isValidExpression &&
+        !invalidSymbolsAsFirstValue.includes(candidateText);
+
+      // if (this.isEmptyExpression()) {
+      //   if (isValidFirstValue) {
+      //     this.expression += candidateText;
+      //   }
+
+      //   return true;
+      // }
+      // dont touch this, idk why it works
+      const hasTwoFactors =
+        Array.from(this.expression).find(
+          (symbol, index) => expressionSymbols.includes(symbol) && index !== 0
+        ) && expressionSymbols.includes(candidateText);
+
+      // if (hasTwoFactors) {
+      //   return;
+      // }
+      // if (isValidExpression) {
+      //   expression.value += candidateText;
+      //   return;
+      // }
+      return true;
+    };
 
     if (isValidExpression(newExpression)) {
       this.expression = newExpression;
